7aa5e4f31f0b27cce09110284b569cf7682884f6
Insan25 <akhmadinsankamil26@gmail.com>
2023-06-07 12:37:22 +0800

    Update welcome_message.php

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
 	<meta charset="utf-8">
-	<title>Welcome to Fatimah</title>
+	<title>Welcome to Testing</title>
 
 	<style type="text/css">
 
