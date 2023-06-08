eaddf272ac6286494ebbb90c4c85b6b36a46636d
Insan25 <akhmadinsankamil26@gmail.com>
2023-06-08 14:10:52 +0800

    Update sidebar.php

--- application/views/sidebar.php
+++ application/views/sidebar.php
@@ -8,10 +8,11 @@
                     <li class="nav-label">Dashboard</li>
                     <li>
                         <a class="has-arrow" href="javascript:void()" aria-expanded="false">
-                            <i class="icon-speedometer menu-icon"></i><span class="nav-text">Dashboard</span>
+                            <i class="icon-speedometer menu-icon"></i><span class="nav-text">Beranda</span>
                         </a>
                         <ul aria-expanded="false">
-                            <li><a href="./index.html">Home 1</a></li>
+                            <li><a href="<?php echo site_url('Barang'); ?>">Data Barang</a></li>
+                            <li><a href="<?php echo site_url(''); ?>">Data Transaksi</a></li>
                             <!-- <li><a href="./index-2.html">Home 2</a></li> -->
                         </ul>
                     </li>
