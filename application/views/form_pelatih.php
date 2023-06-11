d0e11b2248a778eb749c835324762d8bca1c9f6a
dandohz <ramadhani.noor.pratama@gmail.com>
2023-06-11 10:17:07 +0800

    tambah barang

--- application/views/form_pelatih.php
+++ application/views/form_pelatih.php
@@ -4,65 +4,129 @@
         ***********************************-->
         <div class="content-body">
 
-        <div class="container-fluid mt-3">
-                <div class="row page-titles mx-0">
+            
+
+            <div class="row page-titles mx-0">
                 <div class="col p-md-0">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="<?php echo site_url('Barang'); ?>">Data Barang</a></li>
-                        <li class="breadcrumb-item active"><a href="">Tambah</a></li>
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Dashboard</a></li>
+                        <li class="breadcrumb-item active"><a href="javascript:void(0)">Home</a></li>
                     </ol>
                 </div>
-                </div>
+            </div>
+            <!-- row -->
 
-            <div class="container-fluid mt-3">
+            <div class="container-fluid">
                 <div class="row">
-                    <div class="col-lg-12">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="card">
-                                    <div class="card-body">
-                                    <h4 class="card-title">Tambah Barang</h4>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                            <h4 class="card-title">Tambah Barang</h4>
                                 <div class="basic-form">
-                                    <form method="POST" action="<?php echo $action ?>">
-                                        <div class="form">
-                                            <div class="form-group col-md-6">
-                                                <label>Kode Barang</label>
-                                                <input type="text" name="kd_barang" class="form-control" value=" <?php echo $kode_barang ?>">
-                                            </div>
-                                            <div class="form-group col-md-6">
-                                                <label>Nama Barang</label>
-                                                <input type="text" name="nama_barang" class="form-control" value=" <?php echo $nama_barang ?>">
-                                            </div>
-                                            <div class="form-group col-md-6">
-                                                <label>Harga Beli</label>
-                                                <input type="text" name="harga_beli" class="form-control" value="<?php echo $harga_beli ?>">
-                                            </div>
-                                            <div class="form-group col-md-6">
-                                                <label>Harga Jual</label>
-                                                <input type="text" name="harga_jual" class="form-control" value="<?php echo $harga_jual ?>">
-                                            </div>
-                                            <div class="form-group col-md-6">
-                                                <label>Kategori</label>
-                                                <input type="text" name="id_kategori" class="form-control" value="<?php echo $id_kategori ?>">
-                                            </div>
-                                            <button type="submit" class="btn mb-1 btn-primary">Simpan</button>
+                                    <div class="form">
+                                        <div class="form-group col-md-12">
+                                            <label>Kode Barang</label>
+                                            <input type="text" name="kode_barang" class="form-control" value=" <?php echo $kode_barang ?>"
+                                            id="kd_barang" onkeypress="nextfieldBarcode(event)" autofocus>
                                         </div>
-                                    </div>
-                                </form>
-                                </div>
-                                    <div class="chart-wrapper">
-                                        <canvas id="chart_widget_2"></canvas>
+                                        <form method="POST" action="<?php echo $action ?>">
+                                        <div class="form-group col-md-12">
+                                            <label>Nama Barang</label>
+                                            <input type="hidden" name="kd_barang" id="kode_barang">
+                                            <input type="text" name="nama_barang" class="form-control" value=" <?php echo $nama_barang ?>"
+                                            id="nama_barang">
+                                        </div>
+                                        <div class="form-group col-md-12">
+                                            <label>Harga Beli</label>
+                                            <input type="text" name="harga_beli" class="form-control" value="<?php echo $harga_beli ?>">
+                                        </div>
+                                        <div class="form-group col-md-12">
+                                            <label>Harga Jual</label>
+                                            <input type="text" name="harga_jual" class="form-control" value="<?php echo $harga_jual ?>">
+                                        </div>
+                                        <div class="form-group col-md-12">
+                                            <label>Kategori</label>
+                                            <input type="text" name="id_kategori" class="form-control" value="<?php echo $id_kategori ?>">
+                                        </div>
+                                        <div class="form-group col-md-12">
+                                        <button type="submit" class="btn mb-1 btn-primary">Simpan</button>
+                                            <a href="<?= site_url('Barang') ?>" class="btn mb-1 btn-danger">Kembali</a>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                </div>
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-body">
+                            <h4 class="card-title">Data Barang</h4>
+                                <div class="table-responsive">
+                                    <table class="table table-striped table-bordered zero-configuration">
+                                        <thead>
+                                            <tr>
+                                                <th>Kode Barang</th>
+                                                <th>Nama Barang</th>
+                                                <th>Harga Beli</th>
+                                                <th>Harga Jual</th>
+                                                <th>Kategori</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <?php 
+                                            foreach($data_barang as $barang) { ?>
+                                            <tr>
+                                                <td><?php echo $barang->kd_barang; ?></td>
+                                                <td><?php echo $barang->nm_barang; ?></td>
+                                                <td><?php echo $barang->harga_beli; ?></td>
+                                                <td><?php echo $barang->harga_jual; ?></td>
+                                                <td><?php echo $barang->nm_kategori; ?></td>
+                                            </tr>
 
+                                            <?php } ?>
+                                            
+                                            
+                                        </tbody>
+                                        <tfoot>
+                                        <tr>
+                                                <th>Kode Barang</th>
+                                                <th>Nama Barang</th>
+                                                <th>Harga Beli</th>
+                                                <th>Harga Jual</th>
+                                                <th>Kategori</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <!-- #/ container -->
+    
         </div>
+        
         <!--**********************************
             Content body end
         ***********************************-->
+
+        <script>
+            function nextfieldBarcode(event){  // fungsi saat tombol enter
+                if(event.keyCode == 13 || event.which == 13){
+                    var x =  document.getElementById('kd_barang').value
+                    document.getElementById('kode_barang').value = x;
+                    document.getElementById('nama_barang').focus();
+                } 
+            }
+
+            document.getElementById("kd_barang").onchange = function() {myFunction()};
+
+            function myFunction() {
+                var x =  document.getElementById('kd_barang').value
+                    document.getElementById('kode_barang').value = x;
+                    document.getElementById('nama_barang').focus();
+}
+        </script>
         
