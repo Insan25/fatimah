1b610e6bf324371947bb327a68329fe17dbcc292
Insan25 <akhmadinsankamil26@gmail.com>
2023-06-28 14:05:41 +0800

    Create Model_Karyawan.php

---
+++ application/models/Model_Karyawan.php
@@ -0,0 +1,31 @@
+<?php 
+
+class Model_Karyawan extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function get_karyawan_all(){
+
+        $sql = "SELECT karyawan.* from karyawan";
+
+        return $this->db->query($sql)->result();
+    }
+
+    public function delete_karyawan($id_karyawan){
+        $this->db->where('id_karyawan', $id_karyawan);
+        $this->db->delete('karyawan'); 
+    }
+
+    
+    public function insert($data){
+        $this->db->insert('karyawan',$data);
+    }
+
+    public function get_karyawan($id_karyawan)
+    {
+        $this->db->where('id_karyawan',$id_karyawan);
+        return $this->db->get('karyawan')->row();
+    }
+    
+}
