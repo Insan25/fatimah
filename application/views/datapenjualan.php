f82f137f85ec61627103e8a88629afa6a8955913
Insan25 <akhmadinsankamil26@gmail.com>
2023-06-29 16:19:32 +0800

    Create datapenjualan.php

---
+++ application/views/datapenjualan.php
@@ -0,0 +1,85 @@
+
+        <!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+
+            <div class="container-fluid mt-3">
+                <div class="row page-titles mx-0">
+                <div class="col p-md-0">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Beranda</a></li>
+                        <li class="breadcrumb-item active"><a href="javascript:void(0)">Data Penjualan</a></li>
+                    </ol>
+                </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Data Penjualan</h4>
+                                    <a href=<?php echo site_url() ?> ><button type="button" class="btn mb-1 btn-primary">Tambah +</button></a>
+                                    <div class="table-responsive">
+                                    <table class="table table-striped table-bordered zero-configuration">
+                                        <thead>
+                                            <tr>
+                                                <th>No.</th>
+                                                <th>Penjualan ID</th>
+                                                <th>Tanggal</th>
+                                                <th>Pembuat</th>
+                                                <th>Jlh. Barang</th>
+                                                <th>Total</th>
+                                                <th>PPN</th>
+                                                <th>Grand Total</th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td><td>
+                                                <td></td>
+                                                <td></td>
+                                                <td> 
+                                                <a href=<?php echo site_url()?>><button type="button" class="btn btn-sm btn-danger">Hapus</button></a>
+                                                <a href="<?php echo site_url()?>"><button type="button" class="btn btn-sm btn-info">Edit</button></a></td>
+                                            </tr>
+
+                                            
+                                        </tbody>
+                                        <tfoot>
+                                        <tr>
+                                                <th>No.</th>
+                                                <th>Penjualan ID</th>
+                                                <th>Tanggal</th>
+                                                <th>Pembuat</th>
+                                                <th>Jlh. Barang</th>
+                                                <th>Total</th>
+                                                <th>PPN</th>
+                                                <th>Grand Total</th>
+                                                <th>Aksi</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- #/ container -->
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+        
