aa23383057f6383ece57232c071f6316108e95bd
Insan25 <akhmadinsankamil26@gmail.com>
2023-06-30 15:12:28 +0800

    Create laporanpembelian.php

---
+++ application/views/laporanpembelian.php
@@ -0,0 +1,102 @@
+
+        <!--**********************************
+            Content body start
+        ***********************************-->
+        <div class="content-body">
+
+            <div class="container-fluid mt-3">
+                <div class="row page-titles mx-0">
+                <div class="col p-md-0">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Beranda</a></li>
+                        <li class="breadcrumb-item active"><a href="javascript:void(0)">Laporan Pembelian</a></li>
+                    </ol>
+                </div>
+                </div>
+
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="row">
+                            <div class="col-12">
+                                <div class="card">
+                                <div class="card-body">
+                                    <h4 class="card-title">Laporan Pembelian</h4>
+                                    <a href=<?php echo site_url() ?> ><button type="button" class="btn mb-1 btn-primary"><i class="fa fa-print"></i> Cetak</button></a>
+                                    <div class="table-responsive">
+                                    <table class="table table-striped table-bordered zero-configuration">
+                                        <thead>
+                                            <tr>
+                                                <th>No.</th>
+                                                <th>Tahun</th>
+                                                <th>Nama Barang</th>
+                                                <th>Jan</th>
+                                                <th>Feb</th>
+                                                <th>Mar</th>
+                                                <th>Apr</th>
+                                                <th>May</th>
+                                                <th>Jun</th>
+                                                <th>Jul</th>
+                                                <th>Agt</th>
+                                                <th>Sep</th>
+                                                <th>Okt</th>
+                                                <th>Nov</th>
+                                                <th>Des</th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <tr>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                                <td></td>
+                                            </tr>
+
+                                            
+                                        </tbody>
+                                        <tfoot>
+                                        <tr>
+                                                <th>No.</th>
+                                                <th>Tahun</th>
+                                                <th>Nama Barang</th>
+                                                <th>Jan</th>
+                                                <th>Feb</th>
+                                                <th>Mar</th>
+                                                <th>Apr</th>
+                                                <th>May</th>
+                                                <th>Jun</th>
+                                                <th>Jul</th>
+                                                <th>Agt</th>
+                                                <th>Sep</th>
+                                                <th>Okt</th>
+                                                <th>Nov</th>
+                                                <th>Des</th>
+                                            </tr>
+                                        </tfoot>
+                                    </table>
+                                
+                                </div>
+                                </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+            <!-- #/ container -->
+        </div>
+        <!--**********************************
+            Content body end
+        ***********************************-->
+        
