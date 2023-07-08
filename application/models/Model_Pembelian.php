3b18e64fcc3e412e65d714e4e95ee23062f7e46c
Insan25 <akhmadinsankamil26@gmail.com>
2023-07-08 16:30:27 +0800

    Create Model_Pembelian.php

---
+++ application/models/Model_Pembelian.php
@@ -0,0 +1,74 @@
+<?php 
+
+class Model_Pembelian extends CI_Model {
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function get_pembelian_all(){
+
+        $sql = "SELECT *,
+                (SELECT SUM(itempembelian.qty * itempembelian.harga) FROM itempembelian WHERE itempembelian.id_pembelian = pembelian.id_pembelian) AS total,
+                (SELECT COUNT(itempembelian.id_barang) FROM itempembelian WHERE itempembelian.id_pembelian = pembelian.id_pembelian) AS jlh_barang, 
+                (SELECT SUM(itempembelian.qty) FROM itempembelian WHERE itempembelian.id_pembelian = pembelian.id_pembelian) AS jlh_item from pembelian, karyawan
+                WHERE pembelian.id_karyawan = karyawan.id_karyawan";
+
+        return $this->db->query($sql)->result();
+    }
+
+    public function delete_karyawan($id_karyawan){
+        $this->db->where('id_karyawan', $id_karyawan);
+        $this->db->delete('karyawan'); 
+    }
+
+    
+    public function insert($data){
+        $this->db->insert('pembelian',$data);
+    }
+
+    public function insert_item($id_barang, $qty, $harga, $id_pembelian){
+        $this->db->query('INSERT INTO itempembelian(id_barang,qty,harga,id_pembelian) values("' . $id_barang . '",' . $qty . ',' . $harga . ',' . $id_pembelian . ') 
+                            ON DUPLICATE KEY update qty=qty+1');
+
+        return true;
+    }
+
+    public function get_pembelian($id_pembelian)
+    {
+        $sql = "SELECT *,
+                (SELECT SUM(itempembelian.qty * itempembelian.harga) FROM itempembelian WHERE itempembelian.id_pembelian = pembelian.id_pembelian) AS total,
+                (SELECT COUNT(itempembelian.id_barang) FROM itempembelian WHERE itempembelian.id_pembelian = pembelian.id_pembelian) AS jlh_barang, 
+                (SELECT SUM(itempembelian.qty) FROM itempembelian WHERE itempembelian.id_pembelian = pembelian.id_pembelian) AS jlh_item
+                from pembelian, karyawan
+                WHERE pembelian.id_karyawan = karyawan.id_karyawan
+                AND pembelian.id_pembelian = $id_pembelian";
+
+        return $this->db->query($sql)->row();
+    }
+
+    public function get_item_pembelian($id_pembelian){
+
+        $sql = "SELECT * from pembelian, itempembelian, barang
+                WHERE pembelian.id_pembelian = itempembelian.id_pembelian
+                AND barang.kd_barang = itempembelian.id_barang
+                AND itempembelian.id_pembelian = $id_pembelian";
+
+        return $this->db->query($sql)->result();
+    }
+    
+    public function update($id_karyawan, $data)
+    {
+        $this->db->where('id_karyawan', $id_karyawan);
+        $this->db->update('karyawan', $data);
+    }
+
+    public function inqlastid()
+	{   
+       $query = $this->db->query('SELECT LAST_INSERT_ID() as lastid');
+        
+       $res = $query->row();
+       return $res;
+
+	}
+ 
+}
