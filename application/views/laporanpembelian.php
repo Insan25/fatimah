9209a3be03bcd4920fb852936308943437357f2a
Insan25 <akhmadinsankamil26@gmail.com>
2023-07-22 17:50:56 +0800

    Update laporanpembelian.php

--- application/views/laporanpembelian.php
+++ application/views/laporanpembelian.php
@@ -8,7 +8,7 @@
                 <div class="row page-titles mx-0">
                 <div class="col p-md-0">
                     <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="javascript:void(0)">Beranda</a></li>
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Laporan</a></li>
                         <li class="breadcrumb-item active"><a href="javascript:void(0)">Laporan Pembelian</a></li>
                     </ol>
                 </div>
@@ -21,7 +21,6 @@
                                 <div class="card">
                                 <div class="card-body">
                                     <h4 class="card-title">Laporan Pembelian</h4>
-                                    <a href=<?php echo site_url() ?> ><button type="button" class="btn mb-1 btn-primary"><i class="fa fa-print"></i> Cetak</button></a>
                                     <div class="table-responsive">
                                     <table class="table table-striped table-bordered zero-configuration">
                                         <thead>
