283b1d04702a8e0afba7caad9baf44b282130a7b
Insan25 <akhmadinsankamil26@gmail.com>
2023-07-22 13:22:49 +0800

    Create Model_Auth.php

---
+++ application/models/Model_Auth.php
@@ -0,0 +1,27 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Model_Auth extends CI_Model
+{
+    public function testlogin($id_karyawan,$password)
+    {
+     $this->db->where('id_karyawan', $id_karyawan);
+     $this->db->where('password', $password);
+     $query = $this->db->get('karyawan');
+     if ($query->num_rows()>0){
+      foreach ($query->result() as $row) {
+       $sess = array ('id_karyawan' => $row->id_karyawan,
+           'password' => $row->password
+         );
+      }
+     $this->session->set_userdata($sess);
+     redirect('Dashboard');
+     }
+     else{
+      $this->session->set_flashdata('tdkada','MAAF Username dan Password Anda salah!, Mohon diperiksa kembali');
+      redirect('Auth');
+     }
+    }
+   
+}
