47ced11809b645241110bb6980f37d5fc6af13f9
Insan25 <akhmadinsankamil26@gmail.com>
2023-07-23 12:16:47 +0800

    Update Model_Auth.php

--- application/models/Model_Auth.php
+++ application/models/Model_Auth.php
@@ -4,15 +4,15 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Model_Auth extends CI_Model
 {
-    public function testlogin($id_karyawan,$password)
+    public function testlogin($nm_karyawan,$password)
     {
-     $this->db->where('id_karyawan', $id_karyawan);
+     $this->db->where('nm_karyawan', $nm_karyawan);
      $this->db->where('password', $password);
      $query = $this->db->get('karyawan');
     
      if ($query->num_rows()>0){
       foreach ($query->result() as $row) {
-       $sess = array ('id_karyawan' => $row->id_karyawan,
+       $sess = array ('nm_karyawan' => $row->nm_karyawan,
            'password' => $row->password
          );
       }
